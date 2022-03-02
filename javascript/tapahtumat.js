15fb75d2e2a9b38c02dacde3c6f0c3155c115a19
Adnan <adnan.avni@hotmail.com>
2022-03-02 17:57:38 +0200

    tapahtumat pvm korjaus ja fetch kuntoon

--- javascript/tapahtumat.js
+++ javascript/tapahtumat.js
@@ -1,19 +1,22 @@
 'use strict';
+
 let yeet;
+let indeksi = -1;
 const info = document.getElementById('infotapahtuma');
 const seuraavaNappi = document.getElementById('seuraava');
 const edellinenNappi = document.getElementById('edellinen');
 seuraavaNappi.addEventListener('click', () => hakuInfo(yeet));
 edellinenNappi.addEventListener('click', () => hakuInfoNeg(yeet));
+
 // Tapahtumien hakeminen avainsanalla
 const tapahtumaHaku = document.getElementById('hakukentta');
 const tapahtumaHakusana = document.getElementById('hakusana');
 const tapahtumaApi = 'https://api.hel.fi/linkedevents/v1/search/?type=event&q=';
-let indeksi = -1;
+
 
 tapahtumaHaku.addEventListener('submit', function (evt) {
     evt.preventDefault();
-	indeksi = -1;
+    indeksi = -1;
     let search = tapahtumaHakusana.value.split(' ').join('+');
     let tapahtumat = '';
     tapahtumat = tapahtumaApi + search;
@@ -21,11 +24,9 @@ tapahtumaHaku.addEventListener('submit', function (evt) {
     fetch(tapahtumat).then(function (vastaus) {
         return vastaus.json();
     }).then(function (data) {
-		console.log(data)
-		yeet=data;
+        yeet = data;
         indeksi = -1;
         hakuInfo(data);
-        
     }).catch(function (error) {
         console.log(error);
     });
@@ -43,53 +44,52 @@ function hakuInfo(data) {
     while (info.firstChild) {
         info.removeChild(info.firstChild);
     }
-	if(data.data.length){
-    const a = document.createElement('article');
-    info.appendChild(a);
+    if (data.data.length) {
+        const a = document.createElement('article');
+        info.appendChild(a);
 
-    const tulokset = document.createElement('p');
-    tulokset.textContent = "Tuloksia jäljellä: " + (indeksi + 1) + "/" + data.data.length;
-    a.appendChild(tulokset);
+        const tulokset = document.createElement('p');
+        tulokset.textContent = "Tuloksia jäljellä: " + (indeksi + 1) + "/" + data.data.length;
+        a.appendChild(tulokset);
 
-    const nimi = document.createElement('h2');
-    nimi.textContent = 'Tapahtuma: ' + data.data[indeksi].name.fi;
-    a.appendChild(nimi);
+        const nimi = document.createElement('h2');
+        nimi.textContent = 'Tapahtuma: ' + data.data[indeksi].name.fi;
+        a.appendChild(nimi);
 
-    const pvm = document.createElement('p');
-    const paiva = new Date(data.data[indeksi].start_time);
-    let minuutit = `${paiva.getMinutes()}`;
+        const pvm = document.createElement('p');
+        const paiva = new Date(data.data[indeksi].start_time);
+        let minuutit = `${paiva.getMinutes()}`;
 
-    if (minuutit.endsWith('0', 1)) {
-        minuutit += '0';
+        if (minuutit.endsWith('0', 1)) {
+            minuutit += '0';
+        }
+
+        pvm.innerHTML = paiva.getDate() + '.' + (paiva.getMonth() + 1) + '.' + paiva.getFullYear() + ' <br> Klo: ' + paiva.getHours() + '.' + minuutit;
+        a.appendChild(pvm);
+
+        if (data.data[indeksi].short_description != null) {
+            const description = document.createElement('p');
+            description.innerHTML = data.data[indeksi].short_description.fi;
+            a.appendChild(description);
+        }
+
+        if (data.data[indeksi].info_url != null) {
+            const url = document.createElement('a');
+            url.href = data.data[indeksi].info_url.fi
+            url.textContent = 'Lisää tietoa täältä!';
+            a.appendChild(url);
+        }
     }
-
-    pvm.innerHTML = paiva.getDate() + ' / ' + paiva.getMonth() + ' / ' + paiva.getFullYear() + ' <br> Klo: ' + paiva.getHours() + '.' + minuutit;
-    a.appendChild(pvm);
-
-    if (data.data[indeksi].short_description != null) {
-        const description = document.createElement('p');
-        description.innerHTML = data.data[indeksi].short_description.fi;
-        a.appendChild(description);
+    else {
+        const a = document.createElement('article');
+        info.appendChild(a);
+        const h2 = document.createElement('h2');
+        h2.textContent = "Tapahtumia ei löytynyt!";
+        a.appendChild(h2);
     }
-
-    if (data.data[indeksi].info_url != null) {
-        const url = document.createElement('a');
-        url.href = data.data[indeksi].info_url.fi
-        url.textContent = 'Lisää tietoa täältä!';
-        a.appendChild(url);
-    }
-	}
-	else{
-		    const a = document.createElement('article');
-    info.appendChild(a);
-	const h2 = document.createElement('h2');
-    h2.textContent = "Tapahtumia ei löytynyt!";
-    a.appendChild(h2);
-	}
-
 }
 
-// Funktio, joka tulostaa infoboxiin haun ekan tapahtuman, napista edellisen
+// Funktio, joka tulosta edellisen tapahtuman
 function hakuInfoNeg(data) {
     indeksi--;
     if (indeksi > data.data.length - 1) {
@@ -120,7 +120,7 @@ function hakuInfoNeg(data) {
         minuutit += '0';
     }
 
-    pvm.innerHTML = paiva.getDate() + ' / ' + paiva.getMonth() + ' / ' + paiva.getFullYear() + ' <br> Klo: ' + paiva.getHours() + '.' + minuutit;
+    pvm.innerHTML = paiva.getDate() + '.' + paiva.getMonth() + '.' + paiva.getFullYear() + ' <br> Klo: ' + paiva.getHours() + '.' + minuutit;
     a.appendChild(pvm);
 
     if (data.data[indeksi].short_description != null) {
@@ -137,10 +137,67 @@ function hakuInfoNeg(data) {
     }
 }
 
+//Napista pisteet kartalle ja tiedot infoboxiin
 const todayEvents = document.getElementById('tapahtumat')
-todayEvents.addEventListener('click', () => tapahtumatTanaan(), { once: true });
+todayEvents.addEventListener('click', () => tapahtumatTanaan().then(function (jarjestys) {
+    for (let i = 0; i < 100; i++) {
+        lisaaKartalle(jarjestys[i].location.lon, jarjestys[i].location.lat, jarjestys[i].name.fi, tapahtumaIcon).on('click', function () {
 
-//Funktio, joka hakee tämänpäiväiset tapahtumat ja lisää ne kartalle
+            while (info.firstChild) {
+                info.removeChild(info.firstChild);
+            }
+            const a = document.createElement('article');
+            info.appendChild(a);
+
+            const nimi = document.createElement('h2');
+            nimi.textContent = 'Tapahtuma: ' + jarjestys[i].name.fi;
+            a.appendChild(nimi);
+
+            const pvm = document.createElement('p');
+            const paiva = new Date(jarjestys[i].event_dates.starting_day);
+            let minuutit = `${paiva.getMinutes()}`;
+
+            if (minuutit.endsWith('0', 1)) {
+                minuutit += '0';
+            }
+
+            pvm.innerHTML = paiva.getDate() + '.' + (paiva.getMonth() + 1) + '.' + paiva.getFullYear() + ' <br> Klo: ' + paiva.getHours() + '.' + minuutit;
+            a.appendChild(pvm);
+
+            const description = document.createElement('p');
+            description.innerHTML = jarjestys[i].description.intro;
+            a.appendChild(description);
+
+            if (jarjestys[i].info_url != null) {
+                const url = document.createElement('a')
+                url.href = jarjestys[i].info_url
+                url.textContent = 'Lisää tietoa täältä!'
+                a.appendChild(url);
+            }
+
+            const btn = document.createElement('button');
+            btn.textContent = "Näytä reitti";
+            btn.setAttribute("id", "navb");
+            info.appendChild(btn);
+            btn.onclick = function () {
+                navigator.geolocation.getCurrentPosition(success, error, options);
+                function success(pos) {
+                    const crd = pos.coords;
+                    L.Routing.control({
+                        waypoints: [
+                            L.latLng(crd.latitude, crd.longitude),
+                            L.latLng(jarjestys[i].location.lat, jarjestys[i].location.lon)
+
+                        ], router: L.Routing.mapbox('sk.eyJ1IjoibW9pa29ubmEiLCJhIjoiY2t6eTZjMGtlMDhqejJvcGNzanEwcDZhayJ9.an_sHh9hmXUePnTLrVzyFA')
+                    }).addTo(map);
+                }
+            }
+        });
+    }
+
+}), { once: true });
+
+//Funktio, joka hakee tämänpäiväiset tapahtumat
 async function tapahtumatTanaan() {
     const proxy = 'https://api.allorigins.win/get?url=';
     const haku = 'http://open-api.myhelsinki.fi/v1/events/';
@@ -151,60 +208,5 @@ async function tapahtumatTanaan() {
     const tapahtumat = JSON.parse(data.contents);
     const jarjestys = tapahtumat.data.filter(a => a.event_dates.starting_day && new Date().getTime() < new Date(a.event_dates.starting_day)
         .getTime()).sort((a, b) => new Date(a.event_dates.starting_day) - new Date(b.event_dates.starting_day));
-    for (let i = 0; i < 100; i++) {
-        lisaaKartalle(jarjestys[i].location.lon, jarjestys[i].location.lat, jarjestys[i].name.fi, tapahtumaIcon).on('click', () => tanaanInfo(jarjestys[i]));
-    }
-}
-
-// Funktio, joka tulostaa infoboxiin tiedot tapahtumasta, tulostaa ja käynnistää myös navigoinnin
-function tanaanInfo(data) {
-    while (info.firstChild) {
-        info.removeChild(info.firstChild);
-    }
-
-    const a = document.createElement('article');
-    info.appendChild(a);
-
-    const nimi = document.createElement('h2');
-    nimi.textContent = 'Tapahtuma: ' + data.name.fi;
-    a.appendChild(nimi);
-
-    const pvm = document.createElement('p');
-    const paiva = new Date(data.event_dates.starting_day);
-    let minuutit = `${paiva.getMinutes()}`;
-
-    if (minuutit.endsWith('0', 1)) {
-        minuutit += '0';
-    }
-    pvm.innerHTML = paiva.getDate() + ' / ' + paiva.getMonth() + ' / ' + paiva.getFullYear() + ' <br> Klo: ' + paiva.getHours() + '.' + minuutit;
-    a.appendChild(pvm);
-
-    const description = document.createElement('p');
-    description.innerHTML = data.description.intro;
-    a.appendChild(description);
-
-    if (data.info_url != null) {
-        const url = document.createElement('a')
-        url.href = data.info_url
-        url.textContent = 'Lisää tietoa täältä!'
-        a.appendChild(url);
-    }
-
-    const btn = document.createElement('button');
-    btn.textContent = "Näytä reitti";
-    btn.setAttribute("id", "navb");
-    info.appendChild(btn);
-    btn.onclick = function () {
-        navigator.geolocation.getCurrentPosition(success, error, options);
-        function success(pos) {
-            const crd = pos.coords;
-            L.Routing.control({
-                waypoints: [
-                    L.latLng(crd.latitude, crd.longitude),
-                    L.latLng(data.location.lat, data.location.lon)
-
-                ], router: L.Routing.mapbox('sk.eyJ1IjoibW9pa29ubmEiLCJhIjoiY2t6eTZjMGtlMDhqejJvcGNzanEwcDZhayJ9.an_sHh9hmXUePnTLrVzyFA')
-            }).addTo(map);
-        }
-    }
+    return jarjestys;
 }
